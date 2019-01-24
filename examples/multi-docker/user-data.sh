3d34f116fab9669178a4b9ddbff1cabbdc102ece
rafaelmarques7 <rafaelmarques76076@gmail.com>
2019-01-24 11:01:48 +0000

    add multi-docker example

--- examples/multi-docker/user-data.sh
+++ examples/multi-docker/user-data.sh
@@ -22,6 +22,9 @@ git clone https://github.com/rafaelmarques7/multi-docker-hello.git
 # cd to repo
 cd multi-docker-hello/
 
+# get all remote branches
+for b in `git branch -r | grep -v -- '->'`; do git branch --track ${b##origin/} $b; done
+
 # create /etc/nginx directory if not exists
 sudo mkdir -p /etc/nginx
 
